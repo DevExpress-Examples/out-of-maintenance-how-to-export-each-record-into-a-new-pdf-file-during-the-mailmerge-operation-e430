55057b1d13e6e496e60fd0386cb2f9ccdbcbc855
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/MailMergeEachRecordSeparatePDF/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace MailMergeEachRecordSeparatePDF {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.richEditControl1 = new DevExpress.XtraRichEdit.RichEditControl();
+            this.ribbonControl1 = new DevExpress.XtraBars.Ribbon.RibbonControl();
+            this.insertMergeFieldItem1 = new DevExpress.XtraRichEdit.UI.InsertMergeFieldItem();
+            this.showAllFieldCodesItem1 = new DevExpress.XtraRichEdit.UI.ShowAllFieldCodesItem();
+            this.showAllFieldResultsItem1 = new DevExpress.XtraRichEdit.UI.ShowAllFieldResultsItem();
+            this.toggleViewMergedDataItem1 = new DevExpress.XtraRichEdit.UI.ToggleViewMergedDataItem();
+            this.barButtonItem1 = new DevExpress.XtraBars.BarButtonItem();
+            this.mailingsRibbonPage1 = new DevExpress.XtraRichEdit.UI.MailingsRibbonPage();
+            this.mailMergeRibbonPageGroup1 = new DevExpress.XtraRichEdit.UI.MailMergeRibbonPageGroup();
+            this.richEditBarController1 = new DevExpress.XtraRichEdit.UI.RichEditBarController();
+            this.dataNavigator1 = new DevExpress.XtraEditors.DataNavigator();
+            ((System.ComponentModel.ISupportInitialize)(this.ribbonControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.richEditBarController1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richEditControl1
+            // 
+            this.richEditControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richEditControl1.Location = new System.Drawing.Point(0, 169);
+            this.richEditControl1.MenuManager = this.ribbonControl1;
+            this.richEditControl1.Name = "richEditControl1";
+            this.richEditControl1.Size = new System.Drawing.Size(946, 417);
+            this.richEditControl1.TabIndex = 0;
+            this.richEditControl1.Text = "richEditControl1";
+            // 
+            // ribbonControl1
+            // 
+            this.ribbonControl1.ExpandCollapseItem.Id = 0;
+            this.ribbonControl1.ExpandCollapseItem.Name = "";
+            this.ribbonControl1.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.ribbonControl1.ExpandCollapseItem,
+            this.insertMergeFieldItem1,
+            this.showAllFieldCodesItem1,
+            this.showAllFieldResultsItem1,
+            this.toggleViewMergedDataItem1,
+            this.barButtonItem1});
+            this.ribbonControl1.Location = new System.Drawing.Point(0, 0);
+            this.ribbonControl1.MaxItemId = 6;
+            this.ribbonControl1.Name = "ribbonControl1";
+            this.ribbonControl1.Pages.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPage[] {
+            this.mailingsRibbonPage1});
+            this.ribbonControl1.Size = new System.Drawing.Size(946, 142);
+            // 
+            // insertMergeFieldItem1
+            // 
+            this.insertMergeFieldItem1.Id = 2;
+            this.insertMergeFieldItem1.Name = "insertMergeFieldItem1";
+            // 
+            // showAllFieldCodesItem1
+            // 
+            this.showAllFieldCodesItem1.Id = 3;
+            this.showAllFieldCodesItem1.Name = "showAllFieldCodesItem1";
+            // 
+            // showAllFieldResultsItem1
+            // 
+            this.showAllFieldResultsItem1.Id = 4;
+            this.showAllFieldResultsItem1.Name = "showAllFieldResultsItem1";
+            // 
+            // toggleViewMergedDataItem1
+            // 
+            this.toggleViewMergedDataItem1.Id = 5;
+            this.toggleViewMergedDataItem1.Name = "toggleViewMergedDataItem1";
+            // 
+            // barButtonItem1
+            // 
+            this.barButtonItem1.Caption = "Export To PDF";
+            this.barButtonItem1.Id = 1;
+            this.barButtonItem1.Name = "barButtonItem1";
+            this.barButtonItem1.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.barButtonItem1_ItemClick);
+            // 
+            // mailingsRibbonPage1
+            // 
+            this.mailingsRibbonPage1.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
+            this.mailMergeRibbonPageGroup1});
+            this.mailingsRibbonPage1.Name = "mailingsRibbonPage1";
+            // 
+            // mailMergeRibbonPageGroup1
+            // 
+            this.mailMergeRibbonPageGroup1.ItemLinks.Add(this.insertMergeFieldItem1);
+            this.mailMergeRibbonPageGroup1.ItemLinks.Add(this.showAllFieldCodesItem1);
+            this.mailMergeRibbonPageGroup1.ItemLinks.Add(this.showAllFieldResultsItem1);
+            this.mailMergeRibbonPageGroup1.ItemLinks.Add(this.toggleViewMergedDataItem1);
+            this.mailMergeRibbonPageGroup1.ItemLinks.Add(this.barButtonItem1);
+            this.mailMergeRibbonPageGroup1.Name = "mailMergeRibbonPageGroup1";
+            this.mailMergeRibbonPageGroup1.RichEditControl = this.richEditControl1;
+            // 
+            // richEditBarController1
+            // 
+            this.richEditBarController1.BarItems.Add(this.insertMergeFieldItem1);
+            this.richEditBarController1.BarItems.Add(this.showAllFieldCodesItem1);
+            this.richEditBarController1.BarItems.Add(this.showAllFieldResultsItem1);
+            this.richEditBarController1.BarItems.Add(this.toggleViewMergedDataItem1);
+            this.richEditBarController1.RichEditControl = this.richEditControl1;
+            // 
+            // dataNavigator1
+            // 
+            this.dataNavigator1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataNavigator1.Location = new System.Drawing.Point(0, 139);
+            this.dataNavigator1.Name = "dataNavigator1";
+            this.dataNavigator1.Size = new System.Drawing.Size(946, 24);
+            this.dataNavigator1.TabIndex = 2;
+            this.dataNavigator1.Text = "dataNavigator1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(946, 587);
+            this.Controls.Add(this.dataNavigator1);
+            this.Controls.Add(this.richEditControl1);
+            this.Controls.Add(this.ribbonControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ribbonControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.richEditBarController1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraRichEdit.RichEditControl richEditControl1;
+        private DevExpress.XtraBars.Ribbon.RibbonControl ribbonControl1;
+        private DevExpress.XtraRichEdit.UI.InsertMergeFieldItem insertMergeFieldItem1;
+        private DevExpress.XtraRichEdit.UI.ShowAllFieldCodesItem showAllFieldCodesItem1;
+        private DevExpress.XtraRichEdit.UI.ShowAllFieldResultsItem showAllFieldResultsItem1;
+        private DevExpress.XtraRichEdit.UI.ToggleViewMergedDataItem toggleViewMergedDataItem1;
+        private DevExpress.XtraBars.BarButtonItem barButtonItem1;
+        private DevExpress.XtraRichEdit.UI.MailingsRibbonPage mailingsRibbonPage1;
+        private DevExpress.XtraRichEdit.UI.MailMergeRibbonPageGroup mailMergeRibbonPageGroup1;
+        private DevExpress.XtraRichEdit.UI.RichEditBarController richEditBarController1;
+        private DevExpress.XtraEditors.DataNavigator dataNavigator1;
+    }
+}
+
